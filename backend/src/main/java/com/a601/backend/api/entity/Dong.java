cea11e00351c8c042dabc8d79778daa3ecc6d89b
황준원 <miso97o@naver.com>
2022-09-10 18:03:41 +0900

    :sparkles: BE_"엔티티 제작 (구,동 추가)"

--- backend/src/main/java/com/a601/backend/api/entity/Dong.java
+++ backend/src/main/java/com/a601/backend/api/entity/Dong.java
@@ -1,4 +1,36 @@
 package com.a601.backend.api.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Dong {
+    @Id
+    private Long dongId;
+    @Column(nullable = false)
+    private String dong;
+
+    private Long population;
+
+    private Double price;
+
+    private Double traffic_score;
+
+    private Double eating_score;
+
+    private Double culture_score;
+
+    private Double safety_score;
+
+    private Double env_score;
+
+    @JoinColumn(name = "gu_id")
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Gu gu;
 }
--- backend/src/main/java/com/a601/backend/api/entity/Gu.java
+++ backend/src/main/java/com/a601/backend/api/entity/Gu.java
@@ -1,4 +1,22 @@
 package com.a601.backend.api.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class Gu {
+    @Id
+    int guId;
+    @Column(nullable = false)
+    String gu;
+    @OneToMany(mappedBy = "gu", cascade = CascadeType.ALL)
+    private List<Dong> dongList = new ArrayList<>();
 }
