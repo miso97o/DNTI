558cbc1601aed7e36488f2c5e6465ea202123e2b
호인영 <ghdls703@gmail.com>
2022-09-15 13:52:20 +0900

    🐛FE_“수정: 네브바"

--- frontend/src/0_atoms/StyledIcon.jsx
+++ frontend/src/0_atoms/StyledIcon.jsx
@@ -1,3 +1,15 @@
 export default function StyledIcon() {
-    return <h1>StyledIcon</h1>
+  return (
+    <div>
+      <div className="navbarlinktolandingbtn flex-row-vcenter-hcenter">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/m3va4v13wz-I22%3A104%3B28%3A144?alt=media&token=dcf6ef75-51e0-47ce-8479-375341b11cec"
+          alt="Not Found"
+          className="bi-house-door-fill"
+        />
+        <div className="rectangle-54" />
+        <p className="txt-448">동네TI</p>
+      </div>
+    </div>
+  );
 }
