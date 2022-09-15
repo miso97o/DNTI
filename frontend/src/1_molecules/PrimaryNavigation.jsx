bc613a3145dcfbd53f32e0ba10a2d032b60d4192
호인영 <ghdls703@gmail.com>
2022-09-15 13:51:22 +0900

    🐛FE_“수정:  네브바"

--- frontend/src/1_molecules/PrimaryNavigation.jsx
+++ frontend/src/1_molecules/PrimaryNavigation.jsx
@@ -1,14 +1,33 @@
+import styles from "./PrimaryNavigation.module.css";
 import { Link } from "react-router-dom";
 
-export default function PrimaryNavigation() {
+function PrimaryNavigation() {
   return (
     <div>
-      <h1>This is PrimaryNavigation</h1>
-      <Link to={`dnti`}>dnti</Link>
-      <Link to={`dnrecommend`}>동네추천</Link>
-      <Link to={`kmMap`}>1km</Link>
-      <Link to={`myPage`}>마이페이지</Link>
-      <Link to={`board`}>게시판</Link>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Link to={`dnti`}>
+            <p>동네TI</p>
+          </Link>
+        </div>
+        <div className={styles.content}>
+          <Link to={`dnrecommend`}>
+            <p>동네추천</p>
+          </Link>
+        </div>
+        <div className={styles.content}>
+          <Link to={`kmMap`}>
+            <p>1KM</p>          
+          </Link>
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/m3va4v13wz-I22%3A104%3B22%3A44?alt=media&token=1f92b154-97c5-41c3-829c-1cdd04119c37"
+          alt="Not Found"
+          className={styles.image}
+        />
+      </div>
     </div>
   );
 }
+
+export default PrimaryNavigation;
