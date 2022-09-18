7ff90b4c1fc4a721da3666f88da2c90672c510fa
양택훈 <yth0909@nate.com>
2022-09-19 07:51:47 +0900

    :sparkles: BE_make new board branch

--- backend/src/main/java/com/a601/backend/api/service/BoardServiceImpl.java
+++ backend/src/main/java/com/a601/backend/api/service/BoardServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
+
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
