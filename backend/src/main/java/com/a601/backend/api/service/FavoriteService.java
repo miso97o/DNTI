3d37e6b82a26c3b20ca285408e0052e2557e1b9b
박예진 <80744051+yaejin-park@users.noreply.github.com>
2022-09-19 13:50:09 +0900

    :sparkles: Favorite API 수정(순환참조, 유효성검사, pk type 변경)
    즐겨찾기 추가시, 해당 유저의 즐겨찾기 개수를 세서 3개 이하가 아니면 에러 던지게 만듦
    pk를 Integer에서 Long으로 변경(수정, 삭제가 많이 일어나므로 pk 증가 가능성이 크다고 판단)
    즐겨찾기 목록 조회시, entity로 반환해서 순환참조가 일어나는 것을 dto로 반환하게 하여 해결

--- backend/src/main/java/com/a601/backend/api/service/FavoriteService.java
+++ backend/src/main/java/com/a601/backend/api/service/FavoriteService.java
@@ -1,13 +1,15 @@
 package com.a601.backend.api.service;
 
 import com.a601.backend.api.domain.dto.request.FavoriteRequest;
+import com.a601.backend.api.domain.dto.response.FavoriteResponse;
 import com.a601.backend.api.domain.entity.Favorite;
+import com.a601.backend.api.domain.entity.User;
 
 import java.util.List;
 
 public interface FavoriteService {
     //favorite 조회
-    List<Favorite> getFavorite(String userId) throws Exception;
+    List<FavoriteResponse> getFavorite(String userId) throws Exception;
 
     //favorite 추가
     void addFavorite(FavoriteRequest favorite) throws Exception;
@@ -16,5 +18,8 @@ public interface FavoriteService {
     void modifyFavorite(FavoriteRequest favorite) throws Exception;
 
     //favorite 삭제
-    void deleteFavorite(Integer favoriteIde) throws Exception;
+    void deleteFavorite(Long favoriteIde) throws Exception;
+
+    //해당 회원의 즐겨찾기 개수가 3개 이하인지 여부
+    boolean isUnder3(String userId) throws Exception;
 }
