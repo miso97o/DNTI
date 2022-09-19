a59df050ef3c9a7f2d562a7cec6ecd902763da32
황준원 <miso97o@naver.com>
2022-09-19 13:02:34 +0900

    :wrench: BE_"Km 엔티티 생성 및 동 response 수정"

---
+++ backend/src/main/java/com/a601/backend/api/domain/entity/Km.java
@@ -0,0 +1,24 @@
+package com.a601.backend.api.domain.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Km {
+    @Id
+    long baseId;
+
+    String type;
+
+    String name;
+
+    double lat;
+
+    double lng;
+}
