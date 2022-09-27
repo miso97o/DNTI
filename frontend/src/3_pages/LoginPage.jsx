2022ddc6410b4c5709ba5c920c839d44ad07b4df
tttkim <rlaxogk006@naver.com>
2022-09-27 16:44:06 +0900

    :wrench: '프론트엔드 머지 오류 수정'

--- frontend/src/3_pages/LoginPage.jsx
+++ frontend/src/3_pages/LoginPage.jsx
@@ -1,4 +1,14 @@
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import axios from "../utils/axios";
+
+function googleLogIn() {
+  console.log("googleLogIn()");
+  // window.location.replace("http://j7a601.p.ssafy.io/api/google/login");
+  axios.get(`/google/login`).then((data) => {
+    console.log(data);
+  });
+}
 
 function BtnGoogleSignIn() {
   return (
@@ -13,20 +23,11 @@ function BtnGoogleSignIn() {
 
 export default function LoginPage() {
   return (
-    <div className="container mx-auto h-4/5 w-screen flex flex-col justify-content-center place-items-center">
+    <div className="container mx-auto h-full w-screen flex flex-col items-center">
       <h1 className="text-6xl m-10">로그인/회원가입</h1>
       <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=profile%20email%20openid&response_type=code&redirect_uri=http://localhost:9090/api/google/login/redirect&client_id=429814945555-cvulqpkcp494j5n0ujmnnd6slp7ehieh.apps.googleusercontent.com">
-        로그인
+        <BtnGoogleSignIn />
       </a>
-      <BtnGoogleSignIn />
-      <div className="flex flex-col place-items-center">
-        <p>회원가입</p>
-        <Link to={`/signup`} style={{ textDecoration: "none" }}>
-          <div className="h-20 w-20 mx-5">
-            <img src="img/google.png" alt="google signin" />
-          </div>
-        </Link>
-      </div>
     </div>
   );
 }
