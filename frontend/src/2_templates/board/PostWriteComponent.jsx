8ab30960f56dde7242a1aca9133c20a036bab380
황준원 <miso97o@naver.com>
2022-09-29 14:59:01 +0900

    :sparkles: FE/게시글 작성 api 연결

--- frontend/src/2_templates/board/PostWriteComponent.jsx
+++ frontend/src/2_templates/board/PostWriteComponent.jsx
@@ -1,12 +1,40 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import { Button, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function PostWriteComponent() {
   const [postContents, setPostContents] = React.useState("");
-  const handleChange = (event) => {
+  const [postTitle, setPostTitle] = React.useState("");
+  const handleChangeContents = (event) => {
     setPostContents(event.target.value);
   };
+  const handleChangeTitle = (event) => {
+    setPostTitle(event.target.value);
+  };
+  const user = useSelector((state) => state.user);
+  useEffect(() => {
+    if (user.userId == null) {
+      alert("로그인이 필요합니다.");
+      // 로그인되어있지 않은 경우, 로그인 화면으로 redirect
+      navigate("/login", { replace: true });
+    }
+  });
+
+  function writePost() {
+    axios.post(`/board`,{
+      email: user.userId,
+      title: postTitle,
+      contents: postContents
+    })
+    .then((res) => {
+      console.log(res.data);
+      alert("게시글이 작성되었습니다!!")
+    })
+  }
+
   return (
     <div className="flex flex-col w-4/5 h-full items-center m-5">
       <div className="flex flex-row w-full m-5">
@@ -14,13 +42,15 @@ export default function PostWriteComponent() {
       </div>
       <div className="flex flex-row w-full justify-between m-5">
         <div className="flex flex-row w-1/2">
-          <p className="w-1/4">동</p>
-          <p>제목</p>
+          <p className="w-1/4">{user.dong}</p>
+          <TextField
+            value={postTitle}
+            onChange={handleChangeTitle}
+            placeholder="제목을 입력하세요"
+          />
         </div>
         <div className="flex flex-row w-1/2 justify-between">
-          <p>작성자</p>
-          <p>작성일시</p>
-          <p>좋아요</p>
+          <p>{user.nickname}</p>
         </div>
       </div>
       <div className="flex flex-col h-4/5 w-full m-5">
@@ -30,13 +60,14 @@ export default function PostWriteComponent() {
             fullWidth
             minRows={20}
             value={postContents}
-            onChange={handleChange}
+            onChange={handleChangeContents}
+            placeholder="내용을 입력하세요"
           />
         </div>
         <div className="flex flex-row justify-center"></div>
         <div className="flex flex-row justify-center items-center m-5">
           <Link to="/board/postview">
-            <Button>수정</Button>
+            <Button onClick={() => writePost()}>등록</Button>
           </Link>
           <Link to="/board/postview">
             <Button>취소</Button>
