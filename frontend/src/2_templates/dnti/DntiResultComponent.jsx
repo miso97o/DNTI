bae48ff7262084f4d34e67a7cf88cbc9833ce520
박예진 <pyjin805@naver.com>
2022-10-05 00:44:58 +0900

    :lipstick: dnti 결과 등수 넓이 수정

--- frontend/src/2_templates/dnti/DntiResultComponent.jsx
+++ frontend/src/2_templates/dnti/DntiResultComponent.jsx
@@ -141,7 +141,7 @@ function DntiResultCard({ imgsrc, type, content, arr }) {
             </span>
             에게 어울리는 동네는?
           </p>
-          <div className="w-1/4 flex flex-col gap-2">
+          <div className="w-6/12 flex flex-col gap-2">
             {dongList.map((x, idx) => {
               const src = "/img/rank/" + (idx + 1) + ".png";
               return (
